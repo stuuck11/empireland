eaefad9ecc5772365ea11c8f3b9d9a359fdcb712
stuuck11 <mjpelma.cardoso75@gmail.com>
2026-04-01 16:00:24 -0300

    some fixes 9

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import path from "path";
 import * as admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "empireland-5d335",
+  "private_key_id": "1aa520b1167930f1ff79d4d8637a026621bc0e92",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDJ1pdc7E/7SK6v\nEz+QbwR4CFAcdQVEx6WJou0dU6jgxNiX/VZNphiK0JmeCZLMSxZTFynrmD7shTf6\nlAm8Z8ejmWb0VHemMJpvYLf6zq6rFYoQv5JCKNYWXh5m7kkYCwdb+pPxS8h4Lbl0\njvzGUUUqo1v1L241/TiLqvhRL13DQ7c6jZIdIZWWti0yVoEdiyy4ILsZsKVYQ2oI\nroeg1Isdsiyue0AGSKtfDMDN4p5GloyPqreCeVrO2hYtEMzcKvnzvPUCt3eE6vkI\n6cdhwyPRNJd3bS1NcaYl6JP5wzPniQtqH2b4X26wV2Vb2G8XCWvhJRHsUgVzXrS7\nBy+t/NmLAgMBAAECggEAX3dmhIcplXtdVGXQTOqVvIiCC9O1uECOJbmwWYy2dgnl\nQI6NAZEsTow3/K4ehw6E8qwkIdETAiBIx/a5XCniHZlzPuGs7ihTA5dFrhWQPE+l\nzCIwdaaHDHFcA+d1HzAoPPMagqkLmvxVmbIAEXVjN7WoyyGyCUtKMZWeehfjS3Wl\n22Nlq16/n0f1W/xRfC4dvl5gKM6VHcrWw3Q49bYFqrBxcRp6kEk5DC/NprsTq5gK\n51CFMy/Ck5m2OQcYqwEE1EbiAfIlzv7Otaj7arlteP2SWJKXjPBbVOmcX0avZU2x\n9oLjKR2fF4Ke4gB5RmMvQksZ3xtk6d4oJpCHpwBLEQKBgQD7MSEY3YlHoLUX9yq2\nunVKPWoSWHJDD+YcEW06rOxuXrrfMYysVmXSF6/ooDo1jE68sSe52uu1Shih2u2Q\nyvo5/x5W5MFxmadcblkEKW8WlE8icBe5JBr99psBl/TON1bZIkhpDHdTfa7P5/Ly\nbtIAzZ9KfGmn1ClQx8JVq6cSPwKBgQDNs5+Axh/A/IgwNEUADBv2sUCXBasVjqU/\ntYZntg7mWVW2wX7XQKk+BaI8CXI2fkrdEl0phs7sqdWbWEmjvqgWgu8i+XknuGLU\n6DLnKfOOf3wqXqcH+kxdUYCZbXVp3gF6C88ulMWiFZ+pSOQWxZ0Afr/NdtWeV6Ww\n0LnTF6BNtQKBgAeatSXLjxxcAR94nBJsEqDsuwlTJCJjmNPAs03TblTpCT84gMm2\n095IWUrxjtGQLdIIiutVdU1HsPc1aXu4qVqYHpMC2dhWjnp93LQDjRlh5ANm4VD5\htepX4fs0bxscHSiVQ7ZEcO1SlWA8BnmwPTFRM08dvZdteJ8KaXuTGCBAoGBAMlM\nTHzyZcSv3H5/yD5hm0Pw9epUq7Xu4BvWgm8propLkbF1w/QudW6cg+LyNdgx18vbpV\n7EFlIT5MfTMGVRoey9Mr0uWBCWUrddwwVxAM58G9VD8KXZ7a/LwdtCFxEoPFs+BB\nikBlanpReuto33zBagFACGcDMc1ArG658/91kXQ5AoGBAPJ7DZQiLM10LqTpjAdt\nbE2WNLI9BnbEe8yi2RNXIKLIroQXYBNkA17VjdWimKgssDF4VvswWAbqip4BlZfn\ntwGb6+Zh94Y0xJGLPM+klxDZtzbnQdkt0ZZskxfeW5Cfnr5vPf/VqLjfySabFwv7\nD8jGS/vpusr7duPSXsoXa3Fl\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@empireland-5d335.iam.gserviceaccount.com",
+  "client_id": "103391503121359308064",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40empireland-5d335.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
 
 // We'll import Vite dynamically only in development
 // import { createServer as createViteServer } from "vite"; 
